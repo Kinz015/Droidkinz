a14034afd7eadf19b62831036bb5ca340ec83a89
Guilherme Lins <guilhermelins978@gmail.com>
2024-08-21 15:46:43 -0300

    coloquei all em vez de tudo

--- Comandos_Mod/commands_clear.py
+++ Comandos_Mod/commands_clear.py
@@ -1,10 +1,10 @@
 class CommandsClear:
-  def __init__(self, ctx, amount):
+  def __init__(self, ctx, amount=0):
     self.ctx = ctx
     self.amount = amount
 
   async def clear(self):
-    if self.amount == "tudo": 
+    if self.amount == "all": 
       await self.ctx.channel.purge()
     else: 
       await self.ctx.channel.purge(limit=(int(self.amount) + 1))
