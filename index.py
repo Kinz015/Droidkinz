8c7e7b1712bd8884944660dfa07920d742dbd29e
Guilherme Lins <guilhermelins978@gmail.com>
2024-09-24 21:07:45 -0300

    Revert "Merge branch 'main' of https://github.com/Kinz2901/droidkinz"
    This reverts commit 578a5ccc4055b76651cce733cba7b3ff3bbe9737, reversing
    changes made to 0a4e1bcb588d4d6c3479b2320672974b6c257463.

--- index.py
+++ index.py
@@ -6,7 +6,6 @@ import os
 from discord.utils import get
 from Comandos_User.commands_commands import CommandsCommands
 from Comandos_User.commands_profile import CommandsProfile
-from Comandos_User.commands_music import play, skip, stop
 from Comandos_Mod.commands_mute import CommandsMute
 from Comandos_Mod.commands_clear import CommandsClear
 from Comandos_Mod.commands_timeout import CommandsTimeout
@@ -61,11 +60,40 @@ async def profile(ctx, member:discord.Member=False, discord=discord, client=clie
 
 # COMANDOS YT / MUSICA
 
-play()
+@client.command()
+async def play(ctx, *, search):
+    voice_channel = ctx.author.voice.channel if ctx.author.voice else None
+    if not voice_channel.connect:
+      await ctx.channel.send("Você precisa esta conectado a um canal de voz.")
+    if not ctx.voice_client:
+      await voice_channel.connect()
 
-skip()
+    async with ctx.typing():    
+      with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+        info = ydl.extract_info(f"ytsearch:{search}", download=False)
+        if 'entries' in info:
+          info = info['entries'][0]
+        url = info['url']
+        title = info['title']
+        queue.append((url, title))
+        await ctx.send(f'Adicionado a fila: **{title}**')
+    if not ctx.voice_client.is_playing():
+      await play_next(ctx)
 
-stop()
+async def play_next(ctx):
+  if queue:
+    url, title = queue.pop(0)
+    source = await discord.FFmpegOpusAudio.from_probe(url, **FFMPEG_OPTIONS)
+    ctx.voice_client.play(source, after=lambda _:client.loop.create_task(play_next(ctx)))
+    await ctx.send(f"Now playing **{title}**")
+  elif not ctx.voice_client.is_playing():
+    await ctx.send("A fila está vazia!")
+
+@client.command()
+async def skip(ctx):
+  if ctx.voice_client and ctx.voice_client.is_playing():
+    ctx.voice_client.stop()
+    await ctx.send("Proxima")
 
 @client.command()
 async def stop(ctx):
